57e328857106a12da28939f7455c5b6eaa3c68e9
Selvaprasanth-Sellasamy <prasanthsamy61@gmail.com>
2025-12-17 20:59:39 +0530

    Minor UI Changes

--- src/components/notFound/PageNotFound.jsx
+++ src/components/notFound/PageNotFound.jsx
@@ -8,17 +8,20 @@ function PageNotFound(){
     }
 
     return(
-        <main className="h-100 d-flex place-content-center">
-            <section className="w-100 w-max-1000 d-flex justify-space-between align-items-center gap-2">
-                <div>
-                    <h1 className="text-primary fs-7 uppercase mb-5">nomad</h1>
-                    <h2 className="fs-8 font-weight-500">Page not found</h2>
-                    <p className="fs-5 mtb-1 text-secondary">Unfortunately, the page you are looking for does not exist.</p>
-                    <button onClick={handleButtonClick} className="fs-4 font-weight-500 ptb-025 plr-1 rounded-025 bg-green mt-1" type="button">Go to home page</button>
-                </div>
-                <img className="w-50 starting-animation" src={errorImage} alt="not found" />
-            </section>
-        </main>
+        <>
+            <main className="h-100 d-flex place-content-center">
+                <section className="w-100 w-max-1000 d-flex justify-space-between align-items-center gap-2 d-block_L_950">
+                    <div>
+                        <h1 className="text-primary fs-7 uppercase mt-2_L_950">nomad</h1>
+                        <h2 className="fs-9 fs-5_L_500 font-weight-500">Oops...</h2>
+                        <h2 className="fs-7 font-weight-500">Page not found</h2>
+                        <p className="fs-5 mtb-1 text-secondary">Unfortunately, the page you are looking for does not exist.</p>
+                        <button onClick={handleButtonClick} className="fs-4 font-weight-500 ptb-025 plr-1 rounded-025 bg-green mt-1" type="button">Go to home page</button>
+                    </div>
+                    <img className="w-50 w-100_L_950 mt-2_L_950 starting-animation" src={errorImage} alt="not found" />
+                </section>
+            </main>
+        </>
     );
 }
 export default PageNotFound;
